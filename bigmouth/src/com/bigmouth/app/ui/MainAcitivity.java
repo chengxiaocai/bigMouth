7fcfeb969fd38dc06e26e3efd05e8c7e1c5cc580
chengxiaocai <912627598@qq.com>
2015-03-26 19:41:45 +0800

    commit 1

---
+++ bigmouth/src/com/bigmouth/app/ui/MainAcitivity.java
@@ -0,0 +1,42 @@
+package com.bigmouth.app.ui;
+
+import com.yangyu.mytitlebar01.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
+/**
+ * @author yangyu
+ *	功能描述：第二种实现方式,Activity实现方式
+ */
+public class MainAcitivity extends Activity {
+	//定义标题栏上的按钮
+	private ImageButton titleBtn;
+		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_title);
+		
+		initView();			
+	}
+	
+	/**
+	 * 初始化组件
+	 */
+	private void initView(){
+		//实例化标题栏按钮并设置监听
+		titleBtn = (ImageButton) findViewById(R.id.title_btn);
+		titleBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainAcitivity.this,DialogActivity.class));
+			}
+		});						
+	}	
+		
+}
