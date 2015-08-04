dbb9f9e5d5eb4d0e0fe0104b5cd2e71aa33c409c
chengxiaocai <912627598@qq.com>
2015-08-05 01:21:34 +0800

    0121

--- bigmouth/src/com/bigmouth/app/ui/TitlePopup.java
+++ bigmouth/src/com/bigmouth/app/ui/TitlePopup.java
@@ -8,6 +8,8 @@ import cn.sharesdk.framework.Platform;
 import cn.sharesdk.framework.PlatformActionListener;
 import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.sina.weibo.SinaWeibo;
+import cn.sharesdk.tencent.qq.QQ;
 
 import com.bigmouth.app.R;
 import com.bigmouth.app.scan.MipcaCaptureActivity;
@@ -103,6 +105,8 @@ public class TitlePopup extends PopupWindow {
 					OnekeyShare oks = new OnekeyShare();
 					// 关闭sso授权
 					oks.disableSSOWhenAuthorize();
+					oks.addHiddenPlatform(SinaWeibo.NAME);
+					oks.addHiddenPlatform(QQ.NAME);
 
 					// 分享时Notification的图标和文字 2.5.9以后的版本不调用此方法
 					// oks.setNotification(R.drawable.ic_launcher,
@@ -116,7 +120,7 @@ public class TitlePopup extends PopupWindow {
 					// imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
 					oks.setImagePath(filePath);// 确保SDcard下面存在此张图片
 					// url仅在微信（包括好友和朋友圈）中使用
-					oks.setUrl("http://sharesdk.cn");
+				//	oks.setUrl("http://sharesdk.cn");
 					// comment是我对这条分享的评论，仅在人人网和QQ空间使用
 					oks.setComment("我是测试评论文本");
 					// site是分享此内容的网站名称，仅在QQ空间使用
